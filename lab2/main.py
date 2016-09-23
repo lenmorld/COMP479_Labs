c519204b34d5c95c8c81e8d9014186723f4550fe
Lenmor Larroza Dimanalata <l_dimana@encs.concordia.ca>
2016-09-22 20:44:19 -0400

    build inverted index for minimized reuters file

--- lab2/main.py
+++ lab2/main.py
@@ -10,28 +10,51 @@ from sgml_parser import *
 
 empty_words = ['',' ']
 
-## tokenize SGM doc to a list
-lines = extract(open("../reut2-021.sgm"))
-print(lines)
-terms = re.split('\s|(?<!\d)[,.](?!\d)', lines)
-# print(terms)
-terms = [t for t in terms if (not t in empty_words)]
+docs = extract(open("./reut2-021.sgm"))
+doc_ctr = 1
 
-## normalize words
-normalized_words = normalize(terms)
-print("NORMALIZE---------------------------")
-pprint.pprint(normalized_words)
+in_index = {}
 
-## stemming
-stemmer = PorterStemmer()
-tokens = [stemmer.stem(word) for word in normalized_words]
+for title,doc in docs.iteritems():
+    ## tokenize SGM doc to a list
 
-print("STEMMER---------------------------")
-pprint.pprint(tokens)
+    print "Title:" ,title
+    
+    #terms = re.split('\s|(?<!\d)[,.](?!\d)', doc) # same as nltk.word_tokenize()
+    terms = nltk.word_tokenize (doc)
+    
+    # print(terms)
+    terms = [t for t in terms if (not t in empty_words)]
+
+    ## normalize words
+    normalized_words = normalize(terms)
+    print("NORMALIZE---------------------------")
+    #pprint.pprint(normalized_words)
+
+    ## stemming
+    stemmer = PorterStemmer()
+    tokens = [stemmer.stem(word) for word in normalized_words]
+
+    ## build inverted index, docID would stand for each Reuters doc
+
+    print("STEMMER---------------------------")
+    #pprint.pprint(tokens)
+
+    # put in index
+    print("INVERTED INDEX---------------------------")
+    for token in tokens:
+        if token not in in_index:
+            list1 = []
+            list1 += str(doc_ctr)
+            in_index[token] = list1
+        else:
+            in_index[token] =  in_index[token] + list(str(doc_ctr))
+
+    # next doc
+    doc_ctr += 1
 
 
-
-
+pprint.pprint(in_index)
 
 
 
--- lab2/sgml_parser.py
+++ lab2/sgml_parser.py
@@ -1,6 +1,7 @@
 import sgmllib
 import string
 import re
+import pprint
 
 
 # http://www.programcreek.com/python/example/837/sgmllib.SGMLParser
@@ -12,11 +13,33 @@ class FoundTitle(Exception):
 class FoundBody(Exception):
     pass
 
-class ExtractTitle(sgmllib.SGMLParser):
+##class ExtractTitle(sgmllib.SGMLParser):
+##
+##    def __init__(self, verbose=0):
+##        sgmllib.SGMLParser.__init__(self, verbose)
+##        self.title = self.data = None
+##
+##    def handle_data(self, data):
+##        if self.data is not None:
+##            self.data.append(data)
+##
+##    def start_title(self, attrs):
+##        self.data = []
+##
+##    def end_title(self):
+##        self.title = string.join(self.data, "")
+##        raise FoundTitle # abort parsing!
+
+
+class ExtractText(sgmllib.SGMLParser):
+
+    contents = {}
+    reuterAll = 3   # number of body inside the file
+    reuterCtr = 0
 
     def __init__(self, verbose=0):
         sgmllib.SGMLParser.__init__(self, verbose)
-        self.title = self.data = None
+        self.body = self.data = None
 
     def handle_data(self, data):
         if self.data is not None:
@@ -27,41 +50,57 @@ class ExtractTitle(sgmllib.SGMLParser):
 
     def end_title(self):
         self.title = string.join(self.data, "")
-        raise FoundTitle # abort parsing!
-
-
-class ExtractBody(sgmllib.SGMLParser):
-
-    def __init__(self, verbose=0):
-        sgmllib.SGMLParser.__init__(self, verbose)
-        self.body = self.data = None
-
-    def handle_data(self, data):
-        if self.data is not None:
-            self.data.append(data)
+        #print(self.title)
+        self.contents[self.title] = ''
 
     def start_body(self, attrs):
         self.data = []
 
     def end_body(self):
         self.body = string.join(self.data, "")
-        raise FoundBody # abort parsing!
+        self.contents[self.title] = self.body
+        self.reuterCtr += 1
+        print self.reuterCtr
+        
+        if self.reuterCtr == self.reuterAll:   
+            raise FoundBody # abort parsing!
 
 def extract(file):
-    # extract title from an HTML/SGML stream
-    p = ExtractTitle()
-    b = ExtractBody()
-    try:
-        while 1:
-            # read small chunks
-            s = file.read(1024)
-            if not s:
-                break
-            b.feed(s)
-        b.close()
-    except FoundBody:
-        return b.body
-    print None
+    dict_term = {}
+    ctr = 0
+    
+    while True:     
+    
+        # extract title from an HTML/SGML stream
+        b = ExtractText()
+
+##        if p is None:
+##            break
+##        if b is None:
+##            break
+##        
+        try:
+            while 1:
+                # read small chunks
+                s = file.read(1024)
+                if not s:
+                    break
+                b.feed(s)
+            b.close()
+        except FoundBody:
+            return b.contents
+            #return b.body
+            #a = raw_input()
+            #preface = b.body[:100]      # get first 100 char as key
+            #dict_term[preface] =  b.body
+            #print(docs[ctr])
+            ctr += 1
+    return docs
+
+
+
+#docs = extract(open("./reut2-021.sgm"))
+#pprint.pprint(docs)
 
 
 
